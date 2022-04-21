e38abe6c3e3cba678d7441020e7705ea687e7372
jalvarez <jalvarez@orienteed.com>
2022-04-21 11:29:51 +0200

    Show course files

--- packages/venia-concept/@orienteed/customComponents/components/CourseContent/courseContent.js
+++ packages/venia-concept/@orienteed/customComponents/components/CourseContent/courseContent.js
@@ -9,9 +9,19 @@ import { mergeClasses } from '@magento/venia-ui/lib/classify';
 import { useUserContext } from '@magento/peregrine/lib/context/user';
 
 import defaultClasses from './courseContent.module.css';
+
 import getCourseContent from '../../services/getCourseContent';
 import getCourseDetails from '../../services/getCourseDetails';
+
 import noImageAvailable from '../CoursesCatalog/CourseItem/Icons/noImageAvailable.svg';
+import audioIcon from './Icons/audio.svg';
+import fileIcon from './Icons/file.svg';
+import imageIcon from './Icons/image.svg';
+import pdfIcon from './Icons/pdf.svg';
+import urlIcon from './Icons/url.svg';
+import videoIcon from './Icons/video.svg';
+import infoIcon from './Icons/info.svg';
+import notFoundIcon from './Icons/notFound.svg';
 
 const DELIMITER = '/';
 
@@ -29,10 +39,10 @@ const CourseContent = props => {
     const [courseDetails, setCourseDetails] = useState();
     const [courseContent, setCourseContent] = useState();
 
+    console.log(courseContent);
+
     useEffect(() => {
-        getCourseDetails(courseId).then(reply =>
-            setCourseDetails(reply.courses[0])
-        );
+        getCourseDetails(courseId).then(reply => setCourseDetails(reply.courses[0]));
         getCourseContent(courseId).then(reply => setCourseContent(reply));
     }, [courseId]);
 
@@ -49,11 +59,71 @@ const CourseContent = props => {
                 />
             </Link>
             <span className={classes.divider}>{DELIMITER}</span>
-            <span className={classes.currentPage}>
-                {courseDetails.fullname}
-            </span>
+            <span className={classes.currentPage}>{courseDetails.fullname}</span>
         </div>
     );
+
+    const selectIcon = mimetype => {
+        switch (mimetype.split('/')[0]) {
+            case 'audio':
+                return audioIcon;
+            case 'video':
+                return videoIcon;
+            case 'application':
+                return pdfIcon;
+            case 'image':
+                return imageIcon;
+            case 'url':
+                return urlIcon;
+            default:
+                return fileIcon;
+        }
+    };
+
+    const showCourseModules = course => {
+        return (
+            <div className={classes.courseSectionContainer}>
+                <p className={classes.sectionTitle}>{course.name}</p>
+                {course.modules.map(module => {
+                    return (
+                        <div className={classes.courseContentContainer} key={module.id}>
+                            {module.hasOwnProperty('contents') ? (
+                                <div className={classes.courseContentContainerLeft}>
+                                    <img
+                                        src={selectIcon(module?.contents[0].mimetype)}
+                                        className={classes.courseContentIcon}
+                                        alt="file type icon"
+                                    />
+                                    <p className={classes.moduleTitle}>{module.name}</p>
+                                    {module.hasOwnProperty('description') && (
+                                        <img
+                                            src={infoIcon}
+                                            className={classes.courseContentIcon}
+                                            alt={module.description}
+                                        />
+                                    )}
+                                </div>
+                            ) : (
+                                <div className={classes.courseContentContainerLeft}>
+                                    <img
+                                        src={notFoundIcon}
+                                        width="50"
+                                        alt="not found icon"
+                                    />
+                                    <span>
+                                        <FormattedMessage
+                                            id={'lms.notfound'} // TODO_B2B: Translations
+                                            defaultMessage={'Content not available'}
+                                        />
+                                    </span>
+                                </div>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
 
     return (
         <div className={classes.container}>
@@ -64,9 +134,7 @@ const CourseContent = props => {
                         {courseDetails.overviewfiles.length !== 0 ? (
                             <img
                                 className={classes.courseImage}
-                                src={`${
-                                    courseDetails.overviewfiles[0].fileurl
-                                }?token=af547e6e35fca251a48ff4bedb7f1298`}
+                                src={`${courseDetails.overviewfiles[0].fileurl}?token=af547e6e35fca251a48ff4bedb7f1298`}
                                 alt="Course logo"
                             />
                         ) : (
@@ -77,25 +145,25 @@ const CourseContent = props => {
                             />
                         )}
                         <div>
-                            <p className={classes.courseTitle}>
-                                {courseDetails.fullname}
-                            </p>
-                            <p className={classes.summaryText}>
-                                {courseDetails.summary}
-                            </p>
+                            <p className={classes.courseTitle}>{courseDetails.fullname}</p>
+                            <p className={classes.summaryText}>{courseDetails.summary}</p>
                         </div>
                     </div>
                 )}
                 {courseContent !== undefined && (
-                    <div className={classes.bodyCourseContainer}>
-                        {courseContent.map(course => {
-                            return (
-                                course.modules.length !== 0 && (
-                                    <p className={classes.sectionTitle}>{course.name}</p>
-                                )
-                            );
-                        })}
-                    </div>
+                    <>
+                        <span className={classes.contentTitle}>
+                            <FormattedMessage
+                                id={'learning.content'} // TODO_B2B: Translations
+                                defaultMessage={'Content'}
+                            />
+                        </span>
+                        <div className={classes.bodyCourseContainer}>
+                            {courseContent.map(course => {
+                                return course.modules.length !== 0 && showCourseModules(course);
+                            })}
+                        </div>
+                    </>
                 )}
             </div>
         </div>
