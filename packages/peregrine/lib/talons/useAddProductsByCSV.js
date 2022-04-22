85103f22dd48a2e7e8b22fb6dcd85e4317fc0e45
Yousef Yasin <yyasin@orienteed.com>
2022-04-22 09:15:32 +0200

    t=fix toaste err

--- packages/peregrine/lib/talons/useAddProductsByCSV.js
+++ packages/peregrine/lib/talons/useAddProductsByCSV.js
@@ -80,7 +80,6 @@ export const useAddProductsByCSV = props => {
                     dataValidated.push(rawData[i]);
                 }
             } else {
-                setCsvErrorType('fields');
                 setIsCsvDialogOpen(true);
             }
         }
