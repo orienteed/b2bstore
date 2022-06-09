fbb141da83b93d88c5b6c8a5628847795e4ccaa8
Yousef Yasin <yyasin@orienteed.com>
2022-06-09 13:20:41 +0200

    add compare page

--- packages/venia-concept/@orienteed/customComponents/components/comparePage/src/components/CompareProducts.js
+++ packages/venia-concept/@orienteed/customComponents/components/comparePage/src/components/CompareProducts.js
@@ -3,15 +3,13 @@ import useCompareProduct from '@orienteed/customComponents/components/comparePag
 import { FormattedMessage } from 'react-intl';
 import { useStyle } from '@magento/venia-ui/lib/classify';
 
-import ProductsList from './ProductsList';
 import defaultClasses from './compareProducts.module.css';
 import LoadingIndicator from '@magento/venia-ui/lib/components/LoadingIndicator';
-import RichText from '@magento/venia-ui/lib/components/RichText';
+import ProductsTable from './ProductsTable';
 const CompareProducts = () => {
     const talonProps = useCompareProduct();
     const { productsItems, isLoading } = talonProps;
     const classes = useStyle(defaultClasses);
-    console.log(productsItems, 'productsItemsproductsItems');
 
     const productsElements = useMemo(() => {
         if (isLoading) {
@@ -20,60 +18,7 @@ const CompareProducts = () => {
             if (productsItems.length === 0) {
                 return;
             }
-            return (
-                <div className={classes.tableWrapper}>
-                    <table className={classes.productTable}>
-                        <caption className={classes.tableCaption}>Compare Products</caption>
-                        <thead>
-                            <tr>
-                                <th className={classes.cell} scope="row">
-                                    <span> </span>
-                                </th>
-                                {productsItems.map((item, index) => (
-                                    <td className={classes.cell} />
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row" className={`${classes.cell} ${classes.label}`}>
-                                    <span>Product</span>
-                                </th>
-                                {productsItems.map((item, index) => (
-                                    <td className={`${classes.cell} ${classes.info} ${classes.product}`}>
-                                        <ProductsList key={item.id} item={item} />
-                                    </td>
-                                ))}
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <th scope="row" className={`${classes.cell} ${classes.label} `}>
-                                    <span>SKU</span>
-                                </th>
-                                {productsItems.map(({ sku }) => (
-                                    <td className={`${classes.cell} ${classes.info} ${classes.product}`}>
-                                        <span>{sku}</span>
-                                    </td>
-                                ))}
-                            </tr>
-                            <tr>
-                                <th className={`${classes.cell} ${classes.label} `}>
-                                    <span>Description</span>
-                                </th>
-                                {productsItems.map(({ description }) => (
-                                    <td className={`${classes.cell} ${classes.info} ${classes.product}`}>
-                                        <span>
-                                            {' '}
-                                            <RichText content={description.html} />
-                                        </span>
-                                    </td>
-                                ))}
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            );
+            return <ProductsTable productsItems={productsItems} />;
         }
     }, [productsItems]);
     return (
@@ -90,7 +35,6 @@ const CompareProducts = () => {
                 </div>
                 <div> {productsElements} </div>
             </div>
-            {/* {productsElements} */}
         </div>
     );
 };
