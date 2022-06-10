71b0a8b0b12c69a80b621b7be8ab9daaa37340bd
JESUS <jiasenzaniro@orienteed.com>
2022-06-10 17:41:51 +0200

    change the structure for the reorder handler

--- packages/venia-concept/@orienteed/reorder/hooks/useReOrderItems.js
+++ packages/venia-concept/@orienteed/reorder/hooks/useReOrderItems.js
@@ -1,12 +1,13 @@
-import { useMemo, useCallback, useState } from 'react';
+import { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 import { BrowserPersistence } from '@magento/peregrine/lib/util';
+
 const storage = new BrowserPersistence();
 
 // GraphQL mutation to fetch a reorderitems
 const RE_ORDER_ITEMS = gql`
-    mutation reorderItems($orderNumber: String!) {
+    mutation reOrderItems($orderNumber: String!) {
         reorderItems(orderNumber: $orderNumber) {
             cart {
                 id
@@ -15,36 +16,30 @@ const RE_ORDER_ITEMS = gql`
     }
 `;
 
-const useReOrderItems = props => {
+const useReOrderItems = () => {
     const history = useHistory();
 
     const [isLoading, setIsLoading] = useState(false);
 
     // Reorder Data
-    const [reOrderItems, { data }] = useMutation(RE_ORDER_ITEMS);
-    useMemo(() => {
-        if (data && data.reorderItems && data.reorderItems.cart) {
-            storage.setItem('cartId', data.reorderItems.cart.id);
-        }
-    }, [history, data]);
+    const [reOrderItems, { data, loading }] = useMutation(RE_ORDER_ITEMS);
 
-    const handleReOrderClick = useCallback(
-        async orderNumber => {
-            setIsLoading(true);
-            await reOrderItems({
-                variables: {
-                    orderNumber: orderNumber
-                }
-            });
-
-            await history.push('/checkout');
-        },
-        [reOrderItems, setIsLoading]
-    );
+    const handleReOrderClick = async orderNumber => {
+        if (loading) return null;
+        await reOrderItems({
+            variables: {
+                orderNumber: orderNumber
+            }
+        });
+        if (data) storage.setItem('cartId', data.reorderItems.cart.id);
+        history.push('/checkout');
+        location.reload();
+    };
 
     return {
         isLoading,
         handleReOrderClick
     };
 };
+
 export default useReOrderItems;
