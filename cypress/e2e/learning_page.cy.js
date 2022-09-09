f2daa177cebb1ab7894a53e3c5c70299a0eb1701
Miguel de Arcos <mdarcos@orienteed.com>
2022-09-09 09:08:16 +0200

    Added template for learning course test and cart comment

---
+++ cypress/e2e/learning_page.cy.js
@@ -0,0 +1,37 @@
+describe('Learning page test', () => {
+  it('Visits the b2bStore web page', () => {
+    cy.visit('https://b2bstore.orienteed.lan/')
+  })
+
+  it('Click on Create new user and register', () => {
+    cy.get('.panel > .header > .link > a').click()
+    cy.get('.login-container > .block-new-customer > .block-content > .actions-toolbar > div.primary > .action').click()
+
+    // Registration form
+    cy.get('#firstname')
+    .type('Miguel')
+    .should('have.value', 'Miguel')
+
+    cy.get('#lastname')
+    .type('Perez')
+    .should('have.value', 'Perez')
+
+    cy.get('#email_address')
+    .type('test@orienteed.com')
+    .should('have.value', 'test@orienteed.com')
+
+    cy.get('#password')
+    .type('12345678A_')
+
+    cy.get('#password-confirmation')
+    .type('12345678A_')
+
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+    
+  })
+
+
+
+
+})
+
