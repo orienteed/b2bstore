6d7574f9448ba7b60efac97d6ade59d1d7528765
Yousef Yasin <yyasin@orienteed.com>
2022-11-22 09:55:47 +0100

    add routes

--- packages/venia-concept/@orienteed/companyAccount/intercept.js
+++ packages/venia-concept/@orienteed/companyAccount/intercept.js
@@ -7,4 +7,31 @@ module.exports = targets => {
             graphqlQueries: true
         };
     });
+    // Routes
+    process.env.B2BSTORE_VERSION === 'PREMIUM' &&
+        targets.of('@magento/venia-ui').routes.tap(routes => {
+            routes.push(
+                {
+                    name: 'Company Account Information',
+                    pattern: '/company-account/information',
+                    path: '@orienteed/companyAccount/src/components/CompanyInfo/companyInfo'
+                },
+                {
+                    name: 'Company Account Users',
+                    pattern: '/company-account/users',
+                    path: '@orienteed/companyAccount/src/components/CompanyUsers/companyUsers'
+                },
+                {
+                    name: 'Company Account Orders',
+                    pattern: '/company-account/orders',
+                    path: '@orienteed/companyAccount/src/components/CompanyOrders/companyOrders'
+                },
+                {
+                    name: 'Company Account User Roles',
+                    pattern: '/company-account/userRoles',
+                    path: '@orienteed/companyAccount/src/components/UserRoles/userRoles'
+                }
+            );
+            return routes;
+        });
 };
