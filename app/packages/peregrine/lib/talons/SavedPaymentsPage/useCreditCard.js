7cf9ee9daecdca7c047248cb110ae0dfd69ae8b8
dggorienteed <dgonzalez@orienteed.com>
2023-05-18 09:09:42 +0200

    Add deleteCreditCardPayment mutation to adapter

--- app/packages/peregrine/lib/talons/SavedPaymentsPage/useCreditCard.js
+++ app/packages/peregrine/lib/talons/SavedPaymentsPage/useCreditCard.js
@@ -1,18 +1,15 @@
 import { useCallback, useState } from 'react';
-import { useMutation } from '@apollo/client';
 
-import mergeOperations from '@magento/peregrine/lib/util/shallowMerge';
-import DEFAULT_OPERATIONS from './creditCard.gql';
+import { useAdapter } from '@magento/peregrine/lib/hooks/useAdapter';
 
 export const useCreditCard = props => {
     const { paymentHash } = props;
 
-    const operations = mergeOperations(DEFAULT_OPERATIONS, props.operations);
-    const { deleteCreditCardPaymentMutation } = operations;
+    const { deleteCreditCardPayment } = useAdapter();
 
     const [isConfirmingDelete, setIsConfirmingDelete] = useState(false);
 
-    const [deletePayment, { error, loading }] = useMutation(deleteCreditCardPaymentMutation);
+    const {deletePayment, error, loading } = deleteCreditCardPayment();
 
     const handleDeletePayment = useCallback(async () => {
         try {
