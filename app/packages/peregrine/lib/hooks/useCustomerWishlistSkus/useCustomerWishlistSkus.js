0b36390293399e6939a2f099b232a8934b2e5665
dggorienteed <dgonzalez@orienteed.com>
2023-06-07 13:04:20 +0200

    Add getWishlistProductsForLocalField to adapter

--- app/packages/peregrine/lib/hooks/useCustomerWishlistSkus/useCustomerWishlistSkus.js
+++ app/packages/peregrine/lib/hooks/useCustomerWishlistSkus/useCustomerWishlistSkus.js
@@ -1,11 +1,6 @@
 import { useState } from 'react';
-import { useQuery } from '@apollo/client';
 import { useUserContext } from '../../context/user';
 import { useAdapter } from '@magento/peregrine/lib/hooks/useAdapter';
-
-import mergeOperations from '../../util/shallowMerge';
-import DEFAULT_OPERATIONS from './customerWishlist.gql';
-import WISHLIST_OPERATIONS from '../../talons/Wishlist/wishlist.gql';
 
 /**
  * A hook that queries for products in a customer's wishlists and maintains a
@@ -14,10 +9,8 @@ import WISHLIST_OPERATIONS from '../../talons/Wishlist/wishlist.gql';
  * @param {DocumentNode} props.operations operations used to prepare the local customer wishlist array
  * @returns {undefined}
  */
-export const useCustomerWishlistSkus = (props = {}) => {
-    const operations = mergeOperations(DEFAULT_OPERATIONS, WISHLIST_OPERATIONS, props.operations);
-    const { getProductsInWishlistsQuery, getWishlistProductsForLocalFieldQuery } = operations;
-    const { getProductsInWishlists } = useAdapter();
+export const useCustomerWishlistSkus = () => {
+    const { getProductsInWishlists, getWishlistProductsForLocalField } = useAdapter();
 
     const [{ isSignedIn }] = useUserContext();
 
@@ -28,44 +21,13 @@ export const useCustomerWishlistSkus = (props = {}) => {
         data: { customerWishlistProducts }
     } = getProductsInWishlists();
 
-    useQuery(getWishlistProductsForLocalFieldQuery, {
-        fetchPolicy: 'cache-and-network',
-        onCompleted: data => {
-            const itemsToAdd = new Set();
-            const wishlists = data.customer.wishlists;
-            let shouldFetchMore = false;
-            wishlists.map(wishlist => {
-                const items = wishlist.items_v2.items;
-                items.map(item => {
-                    const sku = item.product.sku;
-                    if (!customerWishlistProducts.includes(sku)) {
-                        itemsToAdd.add(sku);
-                    }
-                });
-
-                const pageInfo = wishlist.items_v2.page_info;
-
-                if (pageInfo.total_pages > pageInfo.current_page) {
-                    shouldFetchMore = true;
-                }
-            });
-
-            if (itemsToAdd.size) {
-                client.writeQuery({
-                    query: getProductsInWishlistsQuery,
-                    data: {
-                        customerWishlistProducts: [...customerWishlistProducts, ...itemsToAdd]
-                    }
-                });
-            }
-
-            if (shouldFetchMore) {
-                setCurrentPage(current => ++current);
-            }
-        },
-        skip: !isSignedIn,
-        variables: {
-            currentPage
+    getWishlistProductsForLocalField(
+        {
+            currentPage: currentPage, 
+            client: client, 
+            customerWishlistProducts: customerWishlistProducts, 
+            isSignedIn: isSignedIn, 
+            setCurrentPage: setCurrentPage
         }
-    });
+    );
 };
