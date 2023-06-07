b5e3d5393ea477b1075ec72be50d918a9c7ed9c8
dggorienteed <dgonzalez@orienteed.com>
2023-06-07 12:32:28 +0200

    Add getWishlistProducts query to adapter

--- app/packages/peregrine/lib/talons/Wishlist/wishlist.gql.js
+++ app/packages/peregrine/lib/talons/Wishlist/wishlist.gql.js
@@ -1,29 +1,11 @@
 import { gql } from '@apollo/client';
 
-import { WishlistItemFragment } from './wishlistItemFragments.gql';
 import { WishlistPageFragment } from './wishlistFragment.gql.ce';
 
 export const GET_PRODUCTS_IN_WISHLISTS = gql`
     query GetProductsInWishlists {
         customerWishlistProducts @client
     }
-`;
-
-export const GET_WISHLIST_PRODUCTS = gql`
-    query GetWishlistProducts($id: ID!, $currentPage: Int) {
-        customer {
-            wishlist_v2(id: $id) {
-                id
-                items_v2(currentPage: $currentPage) {
-                    items {
-                        id
-                        ...WishlistItemFragment
-                    }
-                }
-            }
-        }
-    }
-    ${WishlistItemFragment}
 `;
 
 export const REMOVE_PRODUCTS_FROM_WISHLIST = gql`
@@ -50,7 +32,6 @@ export const UPDATE_WISHLIST = gql`
 
 export default {
     getProductsInWishlistsQuery: GET_PRODUCTS_IN_WISHLISTS,
-    getWishlistProductsQuery: GET_WISHLIST_PRODUCTS,
     removeProductsFromWishlistMutation: REMOVE_PRODUCTS_FROM_WISHLIST,
     updateWishlistMutation: UPDATE_WISHLIST
 };
