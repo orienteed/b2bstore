63c48e43095559592dc1d5c2f100313abf9f73d3
dggorienteed <dgonzalez@orienteed.com>
2023-08-07 09:23:57 +0200

    Add getStoreLocations to adapter

--- app/packages/peregrine/lib/talons/StoreLocator/storeLocator.gql.js
+++ app/packages/peregrine/lib/talons/StoreLocator/storeLocator.gql.js
@@ -1,26 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const MP_STORE_LOCATOR_LOCATIONS = gql`
-    query MpStoreLocatorLocations($filter: MpStoreLocatorLocationsFilterInput, $pageSize: Int, $currentPage: Int) {
-        MpStoreLocatorLocations(filter: $filter, pageSize: $pageSize, currentPage: $currentPage) {
-            total_count
-            items {
-                latitude
-                longitude
-                name
-                images
-                email
-                city
-                country
-                street
-                state_province
-                time_zone
-                is_config_time_zone
-            }
-        }
-    }
-`;
-
 export const GET_LOCATIONS_CART = gql`
     query MpStoreLocatorPickupLocationList($cartId: String!) {
         MpStoreLocatorPickupLocationList(cartId: $cartId) {
@@ -72,7 +51,6 @@ const SUBMIT_LOCATION = gql`
 `;
 
 export default {
-    getStoreLocations: MP_STORE_LOCATOR_LOCATIONS,
     getLocationsCart: GET_LOCATIONS_CART,
     getLocationHolidays: GET_LOCATIONS_HOLIDAYS,
     submitLocation: SUBMIT_LOCATION,
